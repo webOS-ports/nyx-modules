21780ae6bad44dce437042a2b3a02ff1141ffc6b
Simon Busch <morphis@gravedo.de>
2012-11-09 21:09:13 +0100

    system: rtc: correctly deal with glib io watches and channels
    This fixes a rather serios problem with rtc poll handling. When a watch is added to the
    glib mainloop it references internally a file descriptor to pass it later to the poll
    function (see poll(2)). Every watch gets an id which should be used by the user to refer
    to the watch whenever needed. When the io channel is destroyed the watch should be remove
    from the mainloop as well. If this isn't done probably by the user it will cause the
    internal list of fds which are passed to the poll function to exceed the maximum of 256.
    This finally results in poll returning EINVAL as error code.
    
    2012-11-09T19:24:24.809509Z [5146] tuna user.warning sleepd: poll(2) failed due to:
    Invalid argument.
    
    GLib doesn't handle this very well and will take 100% CPU usage when this occurs.
    
    This patch adds the correct removal of the watch from the glib mainloop when it's not
    needed anymore. Furthermore the watch isn't added again when it or the channel is still
    valid.
    
    Open-webOS-DCO-1.0-Signed-off-by: Simon Busch <morphis@gravedo.de>

--- src/rockhopper/system/rtc.c
+++ src/rockhopper/system/rtc.c
@@ -161,15 +161,16 @@ static gboolean
 */
 
 static GIOChannel *rtc_channel = NULL;
+static guint rtc_watch;
 
 bool
 	rtc_add_watch(RtcAlarmFunc func)
 {
 #if DEV_RTC_IMPLEMENTED
-	if(rtc_channel == NULL)
-	{
+	if (!rtc_channel)
+    {
 		rtc_channel = g_io_channel_unix_new(rtc_fd);
-		g_io_add_watch(rtc_channel, G_IO_IN, rtc_event, func);
+		rtc_watch = g_io_add_watch(rtc_channel, G_IO_IN, rtc_event, func);
 		g_io_channel_unref(rtc_channel);
 	}
 	return true;
@@ -186,6 +187,9 @@ bool
 	{
 		g_io_channel_shutdown(rtc_channel,true,NULL);
 		rtc_channel=NULL;
+
+		g_source_remove(rtc_watch);
+
 		rtc_close();
 	}
 	return true;
