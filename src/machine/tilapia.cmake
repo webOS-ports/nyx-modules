5bf1e2b74d3b4a71d677a6bcdf0ee02fe17c7055
Simon Busch <morphis@gravedo.de>
2014-02-08 10:32:28 +0100

    grouper/tilapia: choose touchpanel module with mtdev support
    Signed-off-by: Simon Busch <morphis@gravedo.de>

--- src/machine/tilapia.cmake
+++ src/machine/tilapia.cmake
@@ -21,7 +21,7 @@
 
 set(MODULE_SYSTEM_WEBOS_LINUX			YES)
 set(MODULE_KEYS_WEBOS_LINUX			YES)
-set(MODULE_TOUCHPANEL_WEBOS_LINUX		YES)
+set(MODULE_TOUCHPANEL_MTDEV_WEBOS_LINUX		YES)
 
 add_definitions(-DKEYPAD_INPUT_DEVICE=\"/dev/input/event2\")
 add_definitions(-DBATTERY_SYSFS_PATH=\"/sys/class/power_supply/battery/\")
